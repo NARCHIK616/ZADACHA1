8a8814472f8760eee8e65dfb8139ca2a443657e4
narek <narek@DESKTOP-98JS365>
2024-06-19 17:45:28 +0300

    Добавьте файлы проекта.

---
+++ penis negra/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите первое число:");
+        double number1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        double number2 = Convert.ToDouble(Console.ReadLine());
+
+        if (number1 == number2)
+        {
+            Console.WriteLine("Числа равны");
+        }
+        else if (number1 > number2)
+        {
+            Console.WriteLine("Первое число больше второго");
+        }
+        else
+        {
+            Console.WriteLine("Первое число меньше второго");
+        }
+    }
+}
+
